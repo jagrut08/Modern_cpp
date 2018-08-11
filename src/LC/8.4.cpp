bd601c111e64ab061fe8fb2fad52426727657989
jdave12 <jagrut_dave@hotmail.com>
2018-08-10 20:16:47 -0400

    Add function to print frequency table, debug output

--- src/LC/8.4.cpp
+++ src/LC/8.4.cpp
@@ -14,6 +14,8 @@
 void getUniquePermsHelper(std::vector<std::vector<int>>& res, std::vector<int>& cur,
 	std::unordered_map<int, int>& freqTable, const size_t n) {
 	if(cur.size() == n) {
+	//	std::cout << "Result gets ";
+		printVector<int>(cur);
 		res.emplace_back(cur);
 		return;
 	}
@@ -21,10 +23,14 @@ void getUniquePermsHelper(std::vector<std::vector<int>>& res, std::vector<int>& 
 	for(auto iter = freqTable.begin(); iter != freqTable.end(); ++iter) {
 		if(iter->second > 0) {
 			--(iter->second);
+	//		std::cout << "Adding " << std::to_string(iter->first) << '\n';
+	//		printFreqTable<int>(freqTable);
 			cur.emplace_back(iter->first);
 			getUniquePermsHelper(res, cur, freqTable, n);
 			cur.pop_back();
+	//		std::cout << "Removing " << std::to_string(iter->first) << '\n';
 			++(iter->second);
+	//		printFreqTable<int>(freqTable);
 		}
 	}
 }
@@ -45,8 +51,8 @@ int main() {
 //		{1, 2},
 //		{1, 2, 3},
 //		{1, 1},
-//		{1, 1, 2},
-		{1, 1, 2, 2},
+		{1, 1, 2},
+//		{1, 1, 2, 2},
 	};
 	std::for_each(v.begin(), v.end(), getUniquePerms);
 }
--- src/common/misc.h
+++ src/common/misc.h
@@ -7,6 +7,7 @@
 #define SRC_COMMON_MISC_H_
 
 #include <unordered_map>
+#include <sstream>
 
 template <typename T>
 std::unordered_map<T, int> createFreqTableFromVector(const std::vector<T>& v) {
@@ -18,6 +19,23 @@ std::unordered_map<T, int> createFreqTableFromVector(const std::vector<T>& v) {
 		++res[elem];
 	}
 	return res;
+}
+
+template <typename T>
+void printFreqTable(const std::unordered_map<T, int> table) {
+	std::ostringstream ostr;
+	ostr << "Frequency table is\n";
+
+	// The recommended way to pass the <K,V> pairs of the map as per
+	// https://stackoverflow.com/questions/17306715/lambda-signature-for-each-unordered-map
+
+	// Also, the arg to the lambda is a dependent typename
+	auto printLambda = [&ostr](const typename std::unordered_map<T, int>::value_type& p) {
+		ostr << p.first << " : " << p.second << '\n';
+	};
+
+	std::for_each(table.cbegin(), table.cend(), printLambda);
+	std::cout << ostr.str();
 }
 
 #endif /* SRC_COMMON_MISC_H_ */
