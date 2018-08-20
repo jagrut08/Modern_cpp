1465f4c800e3eafa49ac8a456de0848be9093cb1
Jagrut Dave <jdave12@bloomberg.net>
2018-08-20 17:12:04 -0400

    New test code.

--- src/LC/bttest.cpp
+++ src/LC/bttest.cpp
@@ -4,13 +4,24 @@
  */
 
 #include <vector>
+#include <stdexcept>
+
 #include "btnew.h"
 
+
 int main() {
-	const std::vector<int> nodes{1, 2, 3};
-	const int nullVal = -1;
-	auto root = createBT(nodes, nullVal);
-	printBT(root);
+//	const std::vector<int> nodes{1, -1, 2, -1, -1, 4, 4};
+//	const int nullVal = -1;
+
+	const std::vector<char> nodes {'a', '\0', 'c'};
+	const char nullVal = '\0';
+
+	try {
+		auto root = createBT(nodes, nullVal);
+		printBT(root);
+	} catch(const std::invalid_argument& e) {
+		std::cerr << e.what();
+	}
 }
 
 
