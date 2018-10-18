e5aef657a0945bac38cbdfe80a1048d1fdf8c7b3
Jagrut Dave <jdave12@bloomberg.net>
2018-10-18 13:31:43 -0400

    Word ladder bug fix

--- src/LC/9.2.cpp
+++ src/LC/9.2.cpp
@@ -4,8 +4,8 @@ Word Ladder II
 
 Given two words (start and end), and a dictionary, find all shortest transformation sequence(s) from start
 to end, such that:
-• Only one letter can be changed at a time
-• Each intermediate word must exist in the dictionary
+- Only one letter can be changed at a time
+- Each intermediate word must exist in the dictionary
 For example, Given:
 start = "hit"
 end = "cog"
@@ -16,8 +16,8 @@ Return
 ["hit","hot","lot","log","cog"]
 ]
 Note:
-• All words have the same length.
-• All words contain only lowercase alphabetic characters.
+- All words have the same length.
+- All words contain only lowercase alphabetic characters.
  */
 
 #include <print.h>
@@ -42,7 +42,8 @@ std::vector<std::string> getPathToStartWord(const std::string& word, const MapTy
 	}
 	// tmpWord is startWord as startWord hasn't been added to childParentMap
 	res.emplace_back(tmpWord);
-	return std::reverse(res); // Alternately, use a std::list and push_front, then iterate left to right to extract words.
+	std::reverse(res.begin(), res.end()); // Alternately, use a std::list and push_front, then iterate left to right to extract words.
+	return res;
 }
 
 std::vector<std::string> getWordsOneCharAway(const std::string& word, const std::unordered_set<std::string>& dict, const std::string& endWord, const MapType& childParentMap) {
@@ -66,25 +67,33 @@ Complexity
 O(N) time and space, due to BFS
 */
 std::vector<std::vector<std::string>> findShortestTransforms(const std::string& startWord, const std::string& endWord, const std::unordered_set<std::string>& dict) {
+	std::vector<std::vector<std::string>> res;
+
 	if(startWord.size() != endWord.size()) {
 		throw std::runtime_error("startWord and endWord are unequal in size");
+	} else if(startWord == endWord) {
+		res.emplace_back(std::vector<std::string>{startWord});
+		return res;
 	}
 
 	std::list<std::string> curQueue{startWord};
 	MapType childParentMap; // Will hold mapping from childWord -> its parent word and will also be used to check if a
 					        // word has been seen before in the BFS
-	std::vector<std::vector<std::string>> res;
-
 	while(!curQueue.empty() && res.empty()) {
 		std::list<std::string> nextQueue;
 		for(const auto& word : curQueue) {
 			const auto& nextWords = getWordsOneCharAway(word, dict, endWord, childParentMap);
+//			printContainer(nextWords);
 			for(const auto& nextWord : nextWords) {
-				childParentMap[nextWord] = word;
-				nextQueue.emplace_back(nextWord);
-
 				if(nextWord == endWord) {
-					res.emplace_back(getPathToStartWord(nextWord, childParentMap));
+					auto& path = getPathToStartWord(word, childParentMap);
+					path.emplace_back(nextWord);
+					res.emplace_back(path);
+				} else { // Only if the nextWord != endWord, add nextWord to childParentMap and nextQueue
+						// Otherwise only 1 path would be found as the endWord is marked as visited by being in
+					    // childParentMap
+					childParentMap[nextWord] = word;
+					nextQueue.emplace_back(nextWord);
 				}
 			}
 		}
@@ -96,11 +105,10 @@ std::vector<std::vector<std::string>> findShortestTransforms(const std::string& 
 
 int main() {
 	try {
-			const std::string& startWord = "hit", endWord = "cog";
-			const std::unordered_set<std::string> dict {"hot","dot","dog","lot","log"};
+			const std::string& startWord = "hit", endWord = "zzz"; // "hit" "cog"
+			const std::unordered_set<std::string> dict {"hot","dot","dog","lot","log"}; // {}
 			const auto& res = findShortestTransforms(startWord, endWord, dict);
 			printContainer(res);
-
 		} catch(const std::exception& e) {
 			std::cerr << "Exception in main(): " << e.what() << '\n';
 		} catch(...) {
