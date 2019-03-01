a2f179af8f4dd03a3f96c5a811633810eb830a8c
jagrut08 <jagrut_dave@hotmail.com>
2019-03-01 16:01:53 -0500

    Update 12.1.cpp

--- src/LC/12.1.cpp
+++ src/LC/12.1.cpp
@@ -1,12 +1,3 @@
-/*
-Given an array of non-negative integers, you are initially positioned at the first index of the array.
-Each element in the array represents your maximum jump length at that position.
-Determine if you are able to reach the last index.
-For example:
-A = [2,3,1,1,4], return true.
-A = [3,2,1,0,4], return false.
-*/
-
 #include <iostream>
 #include <stdexcept>
 #include <vector>
@@ -33,8 +24,9 @@ bool isLastIndexHelper(std::vector<int>* vPtr, const int idx, const std::vector<
 	return false;
 }
 /*
-O(N) time and space as each element is processed once, and we use a mem table of size N + N stack frames for recursion in worst case ([1, 1, 1, 1]).
-
+O(N^2) time. For each index, we look at all of the indexes on its right to see if they are good (one can reach the last index from them)
+O(N) space complexity as we use a mem table of size N + N stack frames for recursion in worst case (e.g., [1, 1, 1, 1]).
+https://leetcode.com/articles/jump-game/
 */
 bool isLastIndexReacheable(const std::vector<int>& v) {
 	if(v.empty()) {
@@ -44,6 +36,24 @@ bool isLastIndexReacheable(const std::vector<int>& v) {
 	return isLastIndexHelper(&visited, 0, v);
 }
 
+// O(N) time, O(1) space
+bool isLastIndexReacheableOpt(const std::vector<int>& v) {
+    if(v.empty()) {
+        throw std::runtime_error("Empty input");
+    }
+    
+    int idxToReach = v.size() - 1;
+    for(int idx = v.size() - 2; idx >= 0; --idx) {
+        // From idx, you can jump v[idx] to the right, does that enable you to reach the last index?
+        if(idx + v[idx] >= idxToReach) {
+            // If you are able to reach the last index, then this index becomes the index to be reached
+            // Need not try to reach index v.size() - 1 from every location in v, just the next index from which
+            // it's possible to reach v.size() - 1
+            idxToReach = idx;
+        }
+    }
+    return idxToReach == 0;    
+}
 int main() {
     const std::vector<std::vector<int>> inputs {
         {2, 3, 1, 1, 4},
@@ -51,13 +61,14 @@ int main() {
         {1, 0},
         {3, 2, 1, 0},
         {0, 1, 2, 4},
-        {2, 3}
-        
-        
+        {2, 3},
+        {1, 1, 0, 2, 3},
+        {2, 4, 2, 1, 0, 2, 0},
+        {9, 4, 2, 1, 0, 2, 0}
     };
     
     for(const auto& input : inputs) {
-        std::cout << std::boolalpha << isLastIndexReacheable(input) << '\n';
+        std::cout << std::boolalpha << isLastIndexReacheable(input) << " - " << isLastIndexReacheableOpt(input) << '\n';
     }
 }
 
