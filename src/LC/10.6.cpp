171200db8dee68ad1224d124c3dad36601755dc8
jagrut08 <jagrut_dave@hotmail.com>
2019-03-21 15:33:28 -0400

    Update 10.6.cpp

--- src/LC/10.6.cpp
+++ src/LC/10.6.cpp
@@ -1,20 +1,14 @@
+/*
+Given a string containing only digits, restore it by returning all possible valid IP address combinations.
+Input: "25525511135"
+Output: ["255.255.11.135", "255.255.111.35"]
+*/
+
 #include <iostream>
+#include <sstream>
 #include <stdexcept>
 #include <string>
 #include <vector>
-
-std::vector<std::string> convertToIPs(const std::vector<std::vector<int>>& ipsAsInts) {
-	std::vector<std::string> res;
-	for(const auto& ip : ipsAsInts) {
-		std::string ipStr; // TODO can use stringstream
-		for(int i = 0; i <=2; ++i) {
-			ipStr += std::to_string(ip[i]) + ".";	
-		}
-		ipStr += std::to_string(ip[3]);
-		res.emplace_back(ipStr);
-	}
-	return res;
-}
 
 bool isValidOctet(const std::string& str) {
 	if(str.empty()) {
@@ -23,46 +17,52 @@ bool isValidOctet(const std::string& str) {
 	return stoi(str) <= 255;
 }
 
-void getIPAddressesHelper(std::vector<int>* curIpPtr, std::vector<std::vector<int>>* resPtr, const std::string& str, const int offset) {
-	if(!curIpPtr || !resPtr) {
-		throw std::runtime_error("curIpPtr and/or resPtr are null");
+void getIPAddressesHelper(std::vector<std::string>* resPtr, const std::string& curIp, const std::string& str, const int offset, const int octet_num) {
+	if(!resPtr) {
+		throw std::runtime_error("resPtr is null");
 	}
 
-	if(offset == str.size() && curIpPtr->size() == 4) {
-		resPtr->emplace_back(*curIpPtr);
-}  else {
+	if(octet_num == 4) {
+	    if(offset == str.size()) { 
+		    resPtr->emplace_back(curIp);
+	    } 
+	     
+	    return;   
+	}  
+	
 	for(int numChars = 1; numChars <= 3 ; ++numChars) {
 		if(offset + numChars > str.size()) {
 			break;
 		}
 	
-		const std::string& octet = str.substr(offset, numChars);
-	if(isValidOctet(octet)) { // TODO no need to convert str to int if resPtr takes strings
-			curIpPtr->emplace_back(stoi(octet));
-			getIPAddressesHelper(curIpPtr, resPtr, str, offset + numChars);
-		}
-
-	}
-
-}
-
-
+		std::string octet = str.substr(offset, numChars);
+    	if(isValidOctet(octet)) { 
+    	        if(curIp.size()) {
+    	            octet = "." + octet;
+    	        }
+    			getIPAddressesHelper(resPtr, curIp + octet, str, offset + numChars, octet_num + 1);
+    	}
+    }
 }
 
 std::vector<std::string> getIPAddresses(const std::string& str) {
+    std::vector<std::string> res;
     if(str.size() < 4 || str.size() > 12) {
-    	return std::vector<std::string>{};
+    	return res;
     }
     
-    std::vector<int> curIP;
-    std::vector<std::vector<int>> res;
-    getIPAddressesHelper(&curIP, &res, str, 0);
-    return convertToIPs(res);	
+    std::string curIP;
+    getIPAddressesHelper(&res, curIP, str, 0, 0);
+    return res;
 }
 
 int main() {
     const std::vector<std::string> inputs = {
         "25525511135",
+        "1234",
+        "12345",
+        "300123",
+        "123123123123"
     };
     
     for(const auto& input : inputs) {
@@ -70,7 +70,6 @@ int main() {
         for(const auto& res : getIPAddresses(input)) {
             std::cout << res << '\n';   
         }
-        
-           
+        std::cout << '\n';
     }
 }
